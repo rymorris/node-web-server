66d39457ca0f969b1115344ce6bc189db2df82fb
Ryan Morris <rymorris1@gmail.com>
2017-12-10 18:02:27 -0800

    projects page addition

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+  });
+});
 
 app.listen(port, () => {
   console.log('Server running on port ' + port);
